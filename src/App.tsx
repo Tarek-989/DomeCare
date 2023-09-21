38bab05708a825840aebeecbabb1f04bb4353c0b
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-09-22 01:08:50 +0300

    fix navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 import { LoginPage } from './components/LoginPage';
 import { ServicesView } from './components/ServicesView';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+const App: React.FC = () => {
 	const [cookies] = useCookies(['authToken']);
-	const isAuthenticated = !!cookies.authToken;
-
+	const [isAuthenticated, setIsAuthenticated] = useState(!!cookies.authToken);
+	const onLogin = () => {
+		setIsAuthenticated(true);
+	}
 	return (
 		<Router>
 			<ToastContainer />
-			<Switch>
-				<Route path="/login">
-					{isAuthenticated ? <Redirect to="/services" /> : <LoginPage />}
-				</Route>
-				<Route path="/services">
-					{!isAuthenticated ? <Redirect to="/login" /> : <ServicesView />}
-				</Route>
-				<Redirect from="/" to="/login" />
-			</Switch>
+			<Routes>
+				<Route path='/login' element={isAuthenticated ? <Navigate to='/services' /> : <LoginPage onLogin={onLogin} />} />
+				<Route path='/services' element={!isAuthenticated ? <Navigate to='/login' /> : <ServicesView />} />
+				<Route path='/' element={<Navigate to='/login' />} />
+			</Routes>
 		</Router>
 	);
 }
