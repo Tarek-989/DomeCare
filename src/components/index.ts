8928134043f25cb6d7657dcb64fd1ef6a2907f84
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-09-23 10:16:43 +0300

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './AddService';
+export * from './Header';
+export * from './LoginPage';
+export * from './MiddleSection';
+export * from './NavBar';
+export * from './RightSection';
+export * from './Dashboard';
