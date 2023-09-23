0ebb617e72fcdebd9a8665f7e22280e3ad0ee9eb
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-09-23 10:16:33 +0300

    change file name

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,7 +5,7 @@ import { Header } from './Header';
 import { RightSection } from './RightSection';
 import { MiddleSection } from './MiddleSection';
 
-export const ServicesView: React.FC = () => (
+export const Dashboard: React.FC = () => (
 	<Box bgcolor={'#f5f6fa'} width={'100%'} height={'100%'}>
 		<Header />
 		<Box display={'flex'} mt={2} gap={3} paddingX={2}>
